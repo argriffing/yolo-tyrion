5e665ab2133ba01108d8476d66a800b5157ab79f
alex <argriffi@ncsu.edu>
2012-12-14 15:08:08 -0500

    begin looking at bernstein polynomials and approximations

---
+++ check-bernstein-moran-connection.py
@@ -0,0 +1,10 @@
+"""
+Bernstein approximation theory is probably somehow applicable to this puzzle.
+
+But I am not sure how.
+The top of page 103 of
+A Generalized Bernstein Approximation Theorem
+by Miloslav Duchon
+may give a relevant generalization to approximations on a simplex.
+"""
+
